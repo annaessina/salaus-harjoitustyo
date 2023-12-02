edccc20bdf1a33348e59d3c11e3fb6581801957d
Anna Essina <anna.essina@helsinki.fi>
2023-12-02 22:12:30 +0200

    testien korjausta ja viikkoraportti

--- src/tests/test_primeNumberGeneration.py
+++ src/tests/test_primeNumberGeneration.py
@@ -1,12 +1,11 @@
 #This is just beginning skeleton of unittest of primeNumberGeneration().
 #This test will be expanded as work-in-progress.
-#Tested: works fine.
 
 import unittest
 from main import primeNumberGeneration
-from main import checkIfPrimeNumber
+from main import checkIfPrimeNumberMillerRabin
 
 class TestPrimeNumberGeneration(unittest.TestCase):
     def test_primeNumberGeneration(self):
         #Test primeNumberGeneration function
-        self.assertAlmostEqual(checkIfPrimeNumber(primeNumberGeneration(2, 20)), True)
+        self.assertAlmostEqual(checkIfPrimeNumberMillerRabin(primeNumberGeneration(2**1278, 2**2282)), True)
