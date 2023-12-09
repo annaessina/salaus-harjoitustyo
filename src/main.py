e39faa5d1c280e2ac46d2cb6ce1ff6beb1c31081
Anna Essina <anna.essina@helsinki.fi>
2023-12-09 03:05:57 +0200

    main.py päivitys

--- src/main.py
+++ src/main.py
@@ -1,10 +1,37 @@
-
 import math, random
 
+# Numbers for p and q will be choosen randomly. Since p and q should be prime numbers,
+# we have to check whether randomly found number is a prime number
+def checkIfPrimeNumber(num):
+    if num < 2:
+        return False
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            return False
+    return True
+
+# Calculating first 500 prime numbers
+# using Sieve of Eratosthenes algoritm
+def calculate500PrimeNumbers():
+    primeNumber = [True for i in range(3572+2)]
+    i = 2
+    while (i * i <= n):
+        if (primeNumber[i] == True):
+            for j in range(i * i, 3572+1, i):
+                primeNumber[j] = False
+        i += 1
+    list500= [] #list contaning first 500 prime numbers
+    for i in range(2, 3572):
+        if primeNumber[i]:
+            list500.append(i)
+
+
+
+
+
+
 #Miller-Rabin primality test on whether randomly found number is a prime number
- 
 def checkIfPrimeNumberMillerRabin(n):
- 
     s = 0
     k = n-1
 
@@ -27,11 +54,16 @@ def checkIfPrimeNumberMillerRabin(n):
             return False
  
     return True
+
  
+ 
+ 
+
 
 # Generating prime number
 def primeNumberGeneration(num1, num2):
     primeNumber = random.randint(num1, num2)  
+    #while not checkIfPrimeNumber(primeNumber):
     while not checkIfPrimeNumberMillerRabin(primeNumber):
         primeNumber = random.randint(num1, num2)
     return primeNumber
@@ -52,9 +84,16 @@ n = p * q
 #Calculating phi(n) = (p-1)*(q-1)
 phi = (p-1)*(q-1)
 
+# Calculating gcd using Euclidean algorithm
+def calculate_gcd(a, b):
+    while(b):
+       a, b = b, a % b
+    return abs(a)
+
+
 # Choosing e
 e = random.randint(3, phi-1)
-while math.gcd(e, phi) != 1:
+while calculate_gcd(e, phi) != 1:
     e = random.randint(3, phi - 1)
 
 
@@ -89,4 +128,3 @@ m_encoded = [pow(ch, d, n) for ch in c]
 message2 = "".join(chr(ch) for ch in m_encoded)
 
 print("\nYour message decrypted from encrypted form above:", message2)
- 
